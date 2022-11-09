47656891f25614820c04d94c2097c70116b9a8d3
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-11-09 17:25:36 +0100

    add AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>{{ messaggio }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            messaggio: 'La mia prima app con Vite'
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="css" >
+header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 80px;
+    padding: 20px;
+    background-color: chocolate;
+}
+</style>
